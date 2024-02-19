9b9e569bcea0b949124e8b8def0613dfd2068962
fsbym <37413822+fsbym@users.noreply.github.com>
2024-02-18 20:33:00 -0500

    fix nodemodule bugs

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,35 @@
+import "./App.scss";
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Discover from "./pages/Discover";
-import Mint from "./pages/Mint";
-import Profile from "./pages/Profile";
-import User from "./pages/User";
-import NoMatch from "./pages/NoMatch";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Discover from "./pages/Discover";
+// import Mint from "./pages/Mint";
+// import Profile from "./pages/Profile";
+// import User from "./pages/User";
+// import NoMatch from "./pages/NoMatch";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/discover" element={<Discover />} />
-        <Route path="/mint" element={<Mint />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/:id" element={<User />} />
-        <Route path="*" element={<NoMatch />} />
-      </Routes>
-      <Footer />
-    </>
+    <div className="app">
+      <BrowserRouter>
+        <Header />
+        <section className="app__content">
+          <Routes>
+            {/* <Route path="/" element={<Home />} /> */}
+            {/* <Route path="/about" element={<About />} /> */}
+            {/* <Route path="/discover" element={<Discover />} />
+        <Route path="/mint" element={<Mint />} /> */}
+            {/* <Route path="/profile" element={<Profile />} /> */}
+            {/* <Route path="/:id" element={<User />} /> */}
+            {/* <Route path="*" element={<NoMatch />} /> */}
+          </Routes>
+        </section>
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
 
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,44 +1,17 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 export default function Header() {
   return (
-    <>
-      <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Freeform</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Discover</Nav.Link>
-            <Nav.Link href="#features">Mint</Nav.Link>
-            <Nav.Link href="#pricing">My Profile</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      <br />
-      <Navbar bg="primary" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Freeform</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Discover</Nav.Link>
-            <Nav.Link href="#features">Mint</Nav.Link>
-            <Nav.Link href="#pricing">My Profile</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-
-      <br />
-      <Navbar bg="light" data-bs-theme="light">
-        <Container>
-          <Navbar.Brand href="#home">Freeform</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Discover</Nav.Link>
-            <Nav.Link href="#features">Mint</Nav.Link>
-            <Nav.Link href="#pricing">My Profile</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </>
+    <Navbar bg="light" data-bs-theme="light">
+      <Container>
+        <Navbar.Brand href="#home">Freeform</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Discover</Nav.Link>
+          <Nav.Link href="#features">Mint</Nav.Link>
+          <Nav.Link href="#pricing">My Profile</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 }
