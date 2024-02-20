ea3160207b26d1c91d5d8f41c7f94d160a330b54
fsbym <37413822+fsbym@users.noreply.github.com>
2024-02-19 21:31:42 -0500

    add discoverpage html structure pending adjustment

---
+++ client/src/pages/Discover/Discover.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Discover() {
+  const [nftName, setNftName] = useState("");
+  const [nftDescription, setNftDescription] = useState("");
+  const [id] = useParams();
+  return (
+    <section className="discover">
+      <article className="dis">
+        <div className="dis__head">
+          <h2>A short heading</h2>
+        </div>
+        <img src="balloons.jpg" alt="Hot air balloons" />
+        <div className="dis__content">
+          <p>
+            {" "}
+            The idea of reaching the North Pole by means of balloons appears to
+            have been entertained many years ago.{" "}
+          </p>
+        </div>
+      </article>
+    </section>
+  );
+}
