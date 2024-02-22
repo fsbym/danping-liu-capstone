6fa943ad048cc4cad06b50f1a0c8d678c5956228
fsbym <37413822+fsbym@users.noreply.github.com>
2024-02-22 01:47:55 -0500

    change all jsx to js

--- Freeform/frontend/App.jsx
+++ Freeform/frontend/App.jsx
@@ -5,7 +5,7 @@ import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer.jsx"
 // import Home from "./pages/Home";
 // import About from "./pages/About";
-// import Discover from "./pages/Discover";
+import Discover from "./pages/Discover/Discover.jsx"
 // import Mint from "./pages/Mint";
 // import Profile from "./pages/Profile";
 // import User from "./pages/User";
@@ -20,8 +20,8 @@ function App() {
           <Routes>
             {/* <Route path="/" element={<Home />} /> */}
             {/* <Route path="/about" element={<About />} /> */}
-            {/* <Route path="/discover" element={<Discover />} />
-        <Route path="/mint" element={<Mint />} /> */}
+            <Route path="/discover" element={<Discover />} />
+            {/* <Route path="/mint" element={<Mint />} /> */}
             {/* <Route path="/profile" element={<Profile />} /> */}
             {/* <Route path="/:id" element={<User />} /> */}
             {/* <Route path="*" element={<NoMatch />} /> */}
--- Freeform/frontend/components/Header/Header.jsx
+++ Freeform/frontend/components/Header/Header.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import React from "react"
+import { Container, Nav, Navbar } from "react-bootstrap"
 
 export default function Header() {
   return (
     <Navbar bg="light" data-bs-theme="light">
       <Container>
-        <Navbar.Brand href="#home">Freeform</Navbar.Brand>
+        <Navbar.Brand href="/">Freeform</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#home">Discover</Nav.Link>
-          <Nav.Link href="#features">Mint</Nav.Link>
-          <Nav.Link href="#pricing">My Profile</Nav.Link>
+          <Nav.Link href="/discover">Discover</Nav.Link>
+          <Nav.Link href="/mint">Mint</Nav.Link>
+          <Nav.Link href="/ids">My Profile</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
-  );
+  )
 }
