da048e36cdc991b3063b938da474c631e670eec4
fsbym <37413822+fsbym@users.noreply.github.com>
2024-02-24 03:13:32 -0500

    add new file as final

--- Freeform/frontend/components/PriceLable/PriceLable.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function PriceLable(props) {
-  return (
-    <div className="disButtonBase-root disChip-root makeStyles-price-23 disChip-outlined">
-      <span className="disChip-label">{props.sellPrice} ping</span>
-    </div>
-  )
-}
